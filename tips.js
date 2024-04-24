6bd7b0f800956793ef74158c53b768588b02d50e
Clarence Jordan <clarencejordan087@gmail.com>
2024-04-24 10:55:43 -0500

    yeet

---
+++ tips.js
@@ -0,0 +1,8 @@
+// vars create as input for calculation
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.0;
+
+let todalDue = foodCost + tax + top;
+
+console.log(`the total due is ${todalDue}`);
