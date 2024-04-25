735a061daff615ed05b665e2d1d022e0dd7decc1
Clarence Jordan <clarencejordan087@gmail.com>
2024-04-25 14:26:23 -0500

    updated: added comments

--- pay_rules.js
+++ pay_rules.js
@@ -5,19 +5,22 @@ function payRules() {
     let payRate = parseFloat(readline.question("pay rate per hour: "));
     const hoursWorked = parseFloat(readline.question("hours worked:"));
     let totalComp;
-
+    // check if number is valid
     if (!isNaN(payRate) || !isNaN(hoursWorked)) {
       if (hoursWorked >= 40) {
         payRate = payRate * 1.5;
         totalComp = payRate * hoursWorked;
         console.log("total Comp: " + totalComp);
+        // breaks out of the function to avoid loop
         return 0;
       } else {
         totalComp = payRate * hoursWorked;
         console.log("total Comp: " + totalComp);
+        // breaks out of the function to avoid loop
         return 0;
       }
     } else {
+      // error handling
       console.log("Enter valid number or remove symbols and try again ");
       payRules();
     }
@@ -26,4 +29,5 @@ function payRules() {
     payRules();
   }
 }
+// initialize
 payRules();
